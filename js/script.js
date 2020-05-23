fb476cdbd9b99494dc06bc836a7fbc7c9f1f7be3
kuboyoichiro <kubo_yoichiro_xa@cyberagent.co.jp>
2020-05-23 11:55:09 +0900

    feat: hidden pagetop button when initializing

--- js/script.js
+++ js/script.js
@@ -5,16 +5,23 @@ $(function() {
     $answer.slideToggle(400);
   });
 
+  // .pagetopをクリックすると、ページの一番上にアニメーションしながら戻る
   $('.pagetop').click(function() {
-    $('html,body').animate({scrollTop:0},'300');
-  })
+    $('html,body').animate({scrollTop:0}, 300);
+  });
 
-  $('.pagetop').hide();
+  // スクロールした状態でリロードしたら、ボタンがfadeInして出てくる
+  if ($(window).scrollTop() > 0) {
+    $('.pagetop').fadeIn(600);
+  }
+  
+  // スクロールした時の挙動
+  // .pagetopをスクロール位置に応じて出したり消したりしている
   $(window).scroll(function() {
     if($(window).scrollTop() > 0) {
       $('.pagetop').fadeIn(600);
     } else {
       $('.pagetop').fadeOut(600);
     }
-  })
+  });
 });
